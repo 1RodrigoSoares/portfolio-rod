98bbf864fd40b3f1d2e50370d63684dd3b2e5705
Rodrigo Assis <devrodrigosoares@gmail.com>
2025-04-12 19:34:46 -0300

    add page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Rodrigo Assis',
+  icons: {
+    icon: '/favicon-32x32.png',
+  },
 }
 
 export default function RootLayout({
