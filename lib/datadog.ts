4249e8a4802d1a3d2eea2600ee57e64726d59afb
Rodrigo Assis <devrodrigosoares@gmail.com>
2025-04-15 00:51:20 -0300

    trying solutions

--- lib/datadog.ts
+++ lib/datadog.ts
@@ -2,7 +2,17 @@ import { datadogRum } from '@datadog/browser-rum'
 import { reactPlugin } from '@datadog/browser-rum-react'
 
 export function initDatadog() {
-  if (typeof window === 'undefined') return
+  console.log('Initializing Datadog RUM...')
+  if (typeof window === 'undefined')  
+  {
+    console.error('Datadog RUM can only be initialized in the browser')
+    return
+  }
+
+  if (!process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN || !process.env.NEXT_PUBLIC_DATADOG_APP_ID) {
+    console.error('Datadog client token or app ID is not defined')
+    return
+  }
 
   datadogRum.init({
     applicationId: process.env.NEXT_PUBLIC_DATADOG_APP_ID!,
@@ -15,4 +25,8 @@ export function initDatadog() {
     defaultPrivacyLevel: 'mask-user-input',
     plugins: [reactPlugin({ router: true })],
   })
+  
+  console.log('Client Token:', process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN)
+  console.log('App ID:', process.env.NEXT_PUBLIC_DATADOG_APP_ID)
+  console.log('Datadog RUM initialized')
 }
